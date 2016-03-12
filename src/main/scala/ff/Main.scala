50fdb0c4f315afb535d4c2839bde55c9a8f8e9c3
Teo Stocco <teo.stocco@epfl.ch>
2016-03-12 03:25:11 +0100

    works out reading

--- src/main/scala/ff/Main.scala
+++ src/main/scala/ff/Main.scala
@@ -3,6 +3,7 @@ package ff
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
+import akka.util.ByteString
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import io.scalac.amqp.Connection
 
@@ -11,11 +12,23 @@ object Main extends App with LazyLogging {
   implicit val system = ActorSystem("ff")
   implicit val materializer = ActorMaterializer()
 
+  val id = "FlashAndFurious"
+  val accessCode = "zajorsida"
+
+  val START_TEMPLATE = s"/topic/pilots/$id/start"
+  val STOP_TEMPLATE = s"/topic/pilots/$id/stop"
+  val SENSOR_TEMPLATE = s"/topic/pilots/$id/sensor"
+  val VELOCITY_TEMPLATE = s"/topic/pilots/$id/velocity"
+  val PENALTY_TEMPLATE = s"/topic/pilots/$id/penalty"
+  val ROUND_PASSED_TEMPLATE = s"/topic/pilots/$id/roundtime"
+  val ANNOUNCE = s"/app/pilots/announce"
+  val POWER = s"/app/pilots/power"
+
   // streaming invoices to Accounting Department
   val connection = Connection()
 
   // create org.reactivestreams.Publisher
-  val queue = connection.consume(queue = "invoices")
+  val queue = connection.consume(queue = POWER)
 
   // create org.reactivestreams.Subscriber
   val exchange = connection.publish(exchange = "accounting_department", routingKey = "invoices")
@@ -24,9 +37,14 @@ object Main extends App with LazyLogging {
   Source
     .fromPublisher(queue)
     .map { x =>
-      println(x)
+
+      println(ByteString.fromArray(x.message.body.toArray).utf8String)
       x.message
     }
-    .runWith(Sink.fromSubscriber(exchange))
+    .runWith(Sink.ignore)
+
+
+
+  //Sink.fromSubscriber(exchange))
 
 }
