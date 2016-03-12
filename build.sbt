340fcbe5b0a8c2019aa990fbf7bc409651bd01da
Teo Stocco <teo.stocco@epfl.ch>
2016-03-12 02:40:17 +0100

    starting files

--- build.sbt
+++ build.sbt
@@ -3,4 +3,24 @@ name := "FlashAndFurious"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-    
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-feature",
+  "-unchecked",
+  "-Xfatal-warnings",
+  "-Xlint:_"
+)
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.4.2",
+  "com.typesafe.akka" %% "akka-stream" % "2.4.2",
+  "io.scalac" %% "reactive-rabbit" % "1.0.3",
+  "com.typesafe.scala-logging" %%  "scala-logging-slf4j" % "2.1.2",
+  "ch.qos.logback" % "logback-core" % "1.1.6",
+  "ch.qos.logback" % "logback-classic" % "1.1.6",
+  "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+)
+
+cancelable in Global := true
+fork := true
--- src/main/scala/ff/Main.scala
+++ src/main/scala/ff/Main.scala
@@ -1,7 +1,32 @@
 package ff
 
-object Main extends App {
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import io.scalac.amqp.Connection
 
-  println("hello")
+object Main extends App with LazyLogging {
+
+  implicit val system = ActorSystem("ff")
+  implicit val materializer = ActorMaterializer()
+
+  // streaming invoices to Accounting Department
+  val connection = Connection()
+
+  // create org.reactivestreams.Publisher
+  val queue = connection.consume(queue = "invoices")
+
+  // create org.reactivestreams.Subscriber
+  val exchange = connection.publish(exchange = "accounting_department", routingKey = "invoices")
+
+  // Run akka-streams with queue as Source and exchange as Sink
+  Source
+    .fromPublisher(queue)
+    .map { x =>
+      println(x)
+      x.message
+    }
+    .runWith(Sink.fromSubscriber(exchange))
 
 }
